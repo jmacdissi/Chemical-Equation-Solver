302ea505e44e5a23e9ba7d11b925d20709491aa4
Josh Macdissi <jmacdissi@gmail.com>
2016-09-10 18:21:07 -0500

    Fixed repository not working

---
+++ src/org/gmail/jmacdissi/chemicalequationsolver/Main.java
@@ -0,0 +1,13 @@
+package org.gmail.jmacdissi.chemicalequationsolver;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter each element one at a time, ignoring any duplicates");
+		boolean isDone = false;
+		Scanner usrInput = new Scanner(System.in);
+		
+		
+	}
+}
